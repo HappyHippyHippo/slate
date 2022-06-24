5f1110ef1ed0f9546b989b18d10645c3c9fbe498
Carlos Dias <carlos.dias@dreamshaper.com>
2022-06-24 14:43:54 +0100

    rest moved

--- srest/srlog/request_reader_decorator.go
+++
@@ -1,5 +0,0 @@
-package srlog
-
-// RequestReaderDecorator defines a function used to decorate a
-// request reader output.
-type RequestReaderDecorator func(reader RequestReader, model interface{}) (RequestReader, error)
